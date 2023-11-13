b27b73058f150720e7f2379096d86b93b2797936
Andy Bartkiv <andy.bartkiv@accedo.tv>
2023-11-13 03:03:04 -0500

    Initial Commit

---
+++ swift-splash-screen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-splash-screen
+//
+//  Created by Andy Bartkiv on 2023-11-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-splash-screen/swift_splash_screenApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_splash_screenApp.swift
+//  swift-splash-screen
+//
+//  Created by Andy Bartkiv on 2023-11-13.
+//
+
+import SwiftUI
+
+@main
+struct swift_splash_screenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
